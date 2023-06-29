16f879bd6e6b9b2e4a93c0042191dae189f7f489
khanhnk <khanhnk@vega.com.vn>
2023-06-29 15:50:22 +0700

    init

--- app/src/main/java/com/rbservice/yaga/base/FaDialog.kt
+++ app/src/main/java/com/rbservice/yaga/base/FaDialog.kt
@@ -8,14 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.viewbinding.ViewBinding
+import com.rbservice.yaga.R
 
 abstract class FaDialog : DialogFragment() {
     private var binding: ViewBinding? = null
     var baseAction: BaseAction? = null
 
-    init {
-        isCancelable = true
-    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -27,6 +25,9 @@ abstract class FaDialog : DialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireDialog().setCanceledOnTouchOutside(true)
+        requireDialog().window?.attributes?.windowAnimations = R.style.DialogAnimation
+        requireDialog().window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         if (binding != null) {
             initView(savedInstanceState, binding!!)
             baseAction?.getData()
--- app/src/main/java/com/rbservice/yaga/ui/auth/sign_in/SignInFragment.kt
+++ app/src/main/java/com/rbservice/yaga/ui/auth/sign_in/SignInFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.viewbinding.ViewBinding
+import com.hbb20.CountryCodePicker
 import com.rbservice.yaga.R
 import com.rbservice.yaga.base.BaseAction
 import com.rbservice.yaga.base.FaFragment
@@ -11,11 +12,13 @@ import com.rbservice.yaga.databinding.FragmentSignInBinding
 
 class SignInFragment : FaFragment(), BaseAction {
     private lateinit var binding: FragmentSignInBinding
+
+    private lateinit var ccp: CountryCodePicker
     override fun getBindingView(container: ViewGroup?): ViewBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_sign_in, container, false)
 
     override fun initView(savedInstanceState: Bundle?, binding: ViewBinding) {
         this.binding = binding as FragmentSignInBinding
-
+        ccp
         baseAction = this
     }
 
